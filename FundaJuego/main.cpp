6f399c959c7be48f963d5fe8d7b174f650f694e9
Marce Rico <marcelarico@marcelas-macbook-pro.local>
2016-11-01 10:32:52 -0600

    Initial Commit

---
+++ FundaJuego/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FundaJuego
+//
+//  Created by Marcela Rico on 11/1/16.
+//  Copyright © 2016 Marcela Rico. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
